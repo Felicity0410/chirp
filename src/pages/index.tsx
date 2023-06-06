80581278a9f94293f28b82820b849b3744a2166a
Felicity Liu <felicityliu0410@gmail.com>
2023-06-06 16:56:16 +0930

    fix the bug with profileImageUrl in user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,13 +12,14 @@ dayjs.extend(relativeTime)
 
 const CreatePostWizard = () => {
   const user = useUser();
+  console.log(user);
 
   if (!user) return null;
 
   return (
     <div className="flex w-full gap-3 ">
       <Image
-        src={user.profileImageUrl}
+        src={user.user.profileImageUrl}
         alt="Profile image"
         className="h-14 w-14 rounded-full"
         width={56}
